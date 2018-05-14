9f2a1fe639a491f540dc6276112a52626cf2ef1a
Ricard Zapater Muñoz <ricard_zm@hotmail.com>
2018-05-14 11:06:30 +0200

    Base personal page

---
+++ PersonalPage/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from PersonalPage import views
+
+urlpatterns = [
+    path('', views.home, name="personal_home_page"),
+    path('sell_recording', views.sell_recording, name="sell_recording"),
+    path('edit_recording', views.edit_recording, name="edit_recording")
+
+]
