7f382af09da0bbdbf6dd88febc2d0bc83bbed626
m9f2c <mfilellacadens@gmail.com>
2018-05-24 18:58:39 -0700

    #5:
     - Add Ajax interaction (custom.js)
     - Changes in html.html
     - Changes in correct_add.html
     - Changes in delete_my_recording.html
     - Changes in edit_my_recording.html
     - Changes in edit_recording.html
     - Changes in PersonalPage.html
     - Changes in sell_recording.html
     - Add css rules

--- static/html/js/custom.js
+++ static/html/js/custom.js
@@ -1,39 +1,102 @@
 
 $(document).ready(function() {
     /* Django jquery code here */
-    console.log("ready");
+    var idArtist = "none";
+
+    $('#artist_rec').val("");
+    $('#rec_rec').val("");
+
+    /* ARTIST FIELD AJAX */
     $('#artist_rec').autocomplete({
 
         source: function (request, response) {
             var artist_rec = document.getElementById("artist_rec");
+            var val = " ";
+
+            if(artist_rec){
+                val = $(artist_rec).val();
+            }
+
             $.ajax({
                 url: "http://musicbrainz.org/ws/2/artist",
                 type: "GET",
                 data: {
-                    query: $(artist_rec).val()+"*",
-                    limit: "5",
+                    query: val+"*",
                     fmt: "json"
                 },
                 success: function(data) {
-                 console.log( "success" );
                  var nameArray = [];
-                 var i = 0;
                   $.each( data['artists'],function(key,val){
                       nameArray.push({value: val.name , label: val.name , id: val.id});
                    });
+
                    response(nameArray);
                 },
                 error: function(XMLHttpRequest, textStatus, errorThrown){ console.log("ajax fail - " + XMLHttpRequest.responseText)}
             });
         },
-        minLength: 3,
-        select: function( event, ui ) {
-        },
+        minLength: 0,
+        change: function( event, ui ) {
+            idArtist = ui.item.id;
 
+               /* RECORD FIELD AJAX */
+                $('#rec_rec').autocomplete({
+
+                    source: function (request, response) {
+                        $.ajax({
+                            url: "http://musicbrainz.org/ws/2/recording",
+                            type: "GET",
+                            data: {
+                                query: "arid:"+idArtist,
+                                fmt: "json"
+                            },
+                            success: function(data) {
+                                 var nameArray = [];
+
+                                  $.each( data['recordings'],function(key,val){
+                                        var flag = true;
+
+                                        $.each( nameArray,function(key2,val2) {
+
+                                           if(String(val2.value) === String(val.title)) {
+                                               console.log("F A L S E");
+                                                flag = false;
+                                            }
+                                        });
+
+                                        if( flag ) {
+                                            nameArray.push({value: val.title, label: val.title, id: val.id});
+                                        }
+                                   });
+                                  console.log("");
+                                  console.log("");
+                                  console.log("-----------------------------------------");
+                                  console.log("-----------------------------------------");
+                                  console.log("count" + nameArray.length);
+                                  nameArray = jQuery.unique(nameArray);
+                                  console.log("-----------------------------------------");
+                                  console.log(data['recording']);
+                                  console.log(nameArray);
+                                  console.log("count" + nameArray.length);
+                                    $.each( nameArray,function(key,val){
+                                          console.log(val.title);
+                                   });
+                                  console.log("-----------------------------------------");
+                                   response(nameArray);
+                            },
+                        });
+                    },
+                    minLength: 0,
+                    select: function( event, ui ) {
+                    },
+                });
+        },
 
     });
 
-    function update(){
-    }
+
+
+
+
 
 });
